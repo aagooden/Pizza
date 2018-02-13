a825558cd75534e97bcedc91cfc24f3f5c788f56
Aaron Gooden <aagooden@me.com>
2018-02-13 14:11:18 -0500

    First Commit

---
+++ pizza.rb
@@ -0,0 +1,22 @@
+def crust()
+
+crust = ["Thick", "Thin", "Pan", "Stuffed"].sample
+
+end
+
+def meats()
+
+meats = ["Pepperoni", "Sausage", "Ham", "Chicken"].sample
+
+
+end
+
+def veggies()
+
+veggies = ["Peppers", "Onions", "Mushrooms", "Peas"].sample
+
+
+end
+p crust
+p meats
+p veggies
