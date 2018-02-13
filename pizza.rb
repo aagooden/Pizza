d89d53f0cd8f0ddc878dd5fd12d386ff53c7de58
Aaron Gooden <aagooden@me.com>
2018-02-13 14:25:26 -0500

    Added number of pizza selection and print pizzas

--- pizza.rb
+++ pizza.rb
@@ -1,22 +1,26 @@
 def crust()
-
-crust = ["Thick", "Thin", "Pan", "Stuffed"].sample
-
+	crust = ["Thick", "Thin", "Pan", "Stuffed"].sample
 end
 
 def meats()
-
-meats = ["Pepperoni", "Sausage", "Ham", "Chicken"].sample
-
-
+	meats = ["Pepperoni", "Sausage", "Ham", "Chicken"].sample
 end
 
 def veggies()
-
-veggies = ["Peppers", "Onions", "Mushrooms", "Peas"].sample
-
-
+	veggies = ["Peppers", "Onions", "Mushrooms", "Peas"].sample
 end
-p crust
-p meats
-p veggies
+
+def special_sauce()
+	special_sauce = ["Tomato Sauce", "Ranch Sauce", "Itallian Sauce", "Fruit Sauce"].sample
+end
+
+puts "How many pizzas would you like"
+	num_p = gets.chomp
+	num_p = num_p.to_i
+
+for x in 1..num_p
+	puts "Pizza #{x}"
+	print "#{crust}, #{meats}, #{veggies}, #{special_sauce}\n"
+	puts "\n"
+end
+
