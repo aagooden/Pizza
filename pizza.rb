38da7def9f0fef1903edebc477717183df9faf55
Aaron Gooden <aagooden@me.com>
2018-02-13 14:40:57 -0500

    Added Cost Calculation

--- pizza.rb
+++ pizza.rb
@@ -1,5 +1,5 @@
 def crust()
-	crust = ["Thick", "Thin", "Pan", "Stuffed"].sample
+	crust = ["Thick Crust", "Thin Crust", "Pan Crust", "Stuffed Crust"].sample
 end
 
 def meats()
@@ -14,13 +14,19 @@ def special_sauce()
 	special_sauce = ["Tomato Sauce", "Ranch Sauce", "Itallian Sauce", "Fruit Sauce"].sample
 end
 
+def special_tops()
+	special_sauce = ["Extra Cheese", "Extra Sauce", "Pineapple"].sample
+end
+
 puts "How many pizzas would you like"
 	num_p = gets.chomp
 	num_p = num_p.to_i
 
 for x in 1..num_p
 	puts "Pizza #{x}"
-	print "#{crust}, #{meats}, #{veggies}, #{special_sauce}\n"
+	print "#{crust}, #{meats}, #{veggies}, #{special_sauce}, #{special_tops}\n"
 	puts "\n"
 end
+
+puts "Pizzas are $10 each so you owe $#{num_p * 10}.00"
 
