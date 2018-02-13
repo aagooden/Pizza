2b4353ee3c864dbd38d3e93bcec41f0264a48d4e
Aaron Gooden <aagooden@me.com>
2018-02-13 16:58:30 -0500

    Added charge for sizes and tip

--- pizza.rb
+++ pizza.rb
@@ -1,32 +1,49 @@
 def crust()
-	crust = ["Thick Crust", "Thin Crust", "Pan Crust", "Stuffed Crust"].sample
+	["Thick Crust", "Thin Crust", "Pan Crust", "Stuffed Crust"].sample
 end
 
 def meats()
-	meats = ["Pepperoni", "Sausage", "Ham", "Chicken"].sample
+	["Pepperoni", "Sausage", "Ham", "Chicken"].sample
 end
 
 def veggies()
-	veggies = ["Peppers", "Onions", "Mushrooms", "Peas"].sample
+	["Peppers", "Onions", "Mushrooms", "Peas"].sample
 end
 
 def special_sauce()
-	special_sauce = ["Tomato Sauce", "Ranch Sauce", "Itallian Sauce", "Fruit Sauce"].sample
+	["Tomato Sauce", "Ranch Sauce", "Itallian Sauce", "Fruit Sauce"].sample
 end
 
 def special_tops()
-	special_sauce = ["Extra Cheese", "Extra Sauce", "Pineapple"].sample
+	["Extra Cheese", "Extra Sauce", "Pineapple"].sample
+end
+
+def size()
+	$pizza_size = ["Small", "Medium", "Large", "Extra Large"].sample
 end
 
 puts "How many pizzas would you like"
 	num_p = gets.chomp
 	num_p = num_p.to_i
 
+owe = 0
+
 for x in 1..num_p
 	puts "Pizza #{x}"
-	print "#{crust}, #{meats}, #{veggies}, #{special_sauce}, #{special_tops}\n"
+	print "#{size}, #{crust}, #{meats}, #{veggies}, #{special_sauce}, #{special_tops}\n"
+	if $pizza_size == "Small"
+		owe = owe + 8
+	elsif $pizza_size == "Medium"
+		owe = owe + 10
+	elsif $pizza_size == "Large"
+		owe = owe + 12
+	else
+		owe = owe + 15
+	end
 	puts "\n"
 end
 
-puts "Pizzas are $10 each so you owe $#{num_p * 10}.00"
+delivery = num_p * 2
+
+puts "You owe $#{owe + delivery}.00"
 
