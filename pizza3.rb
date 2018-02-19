9fc82ca00869a6b998ddd852d0a7396efd504eb2
Aaron Gooden <aagooden@me.com>
2018-02-19 16:36:50 -0500

    Added pizza ordering and sides ordering

--- pizza3.rb
+++ pizza3.rb
@@ -4,16 +4,16 @@ def database
 	{
 		order_items: [["Pizza", 0], ["Sub", 0], ["Side", 0], ["Drink", 0], ["Dessert", 0]],
 		piz_crusts: [["Thick", 0], ["Thin", 0], ["Regular", 0], ["Stuffed", 2]],
-		piz_meat: [["Pepperoni", 0], ["Sausage", 0], ["Ham", 0], ["Chicken", 0]],
-		piz_veggies: [["Peppers", 0], ["Onions", 0], ["Mushrooms", 0], ["Olives", 0]],
-		piz_sauces: [["Tomato", 0], ["Alfredo", 0], ["No Sauce", 0], ["Spicy", 0]],
-		piz_special_tops: [["Anchovies", 0], ["Pineapple", 0], ["Bacon", 0]],
+		piz_meat: [["Pepperoni", 0], ["Sausage", 0], ["Ham", 0], ["Chicken", 0], ["None", 0]],
+		piz_veggies: [["Peppers", 0], ["Onions", 0], ["Mushrooms", 0], ["Olives", 0], ["None", 0]],
+		piz_sauces: [["Tomato", 0], ["Alfredo", 0], ["Spicy", 0], ["None", 0]],
+		piz_special_tops: [["Anchovies", 0], ["Pineapple", 0], ["Bacon", 0], ["Extra Cheese"], ["None", 0]],
 		piz_sizes: [["Small", 10], ["Medium", 12], ["Large", 15], ["Extra Large", 18]],
 		sub_breads: [["White", 0], ["Whole Wheat", 0], ["Itallian", 0], ["Herb", 0]],
 		sub_sizes: [["6 Inch", 6], ["Foot Long", 10], ["Good Grief", 15]],
 		drink_sizes: [["Small", 2.5], ["Medium", 3], ["Large", 4], ["Extra Large", 6]],
 		drink_types: [["Coke", 0], ["Diet Coke", 0], ["Sprite", 0], ["Dr. Pepper", 0]],
-		side_types: [["Fries", 0], ["Onion Rings", 0], ["Potato Chips", 0]],
+		side_types: [["Fries", 4], ["Onion Rings", 6], ["Potato Chips", 3]],
 		desserts: [["Chocolate Cake", 2], ["Pudding", 1.5], ["Chocolate Chip Cookie", 1]]
 	}
 end
@@ -96,6 +96,32 @@ def dessert_order(order, database, item_count)
 end
 
 
+def sides_order(order, database, item_count)
+
+	system "clear"
+	system "cls"
+	selection = 0
+	puts ""
+	puts "Select your side from the following:"
+
+	for x in (0...database[:side_types].length)
+	puts "#{x+1} - #{database[:side_types][x][0]}: $#{database[:side_types][x][1]}"
+	end
+
+	until selection.between?(1, database[:side_types].length)
+		puts "Enter the number that is next to your selection"
+		selection = gets.chomp
+		selection = selection.to_i 
+	end	
+	item_count += 1
+	order[item_count] = database[:side_types][selection - 1]
+	order[item_count].unshift('Side')
+	
+	
+
+	return [order, item_count]
+end
+
 def calculate_owe(order, owe)
 
 	for x in (1...(order.length + 1))
@@ -104,6 +130,55 @@ def calculate_owe(order, owe)
 	return owe 
 end
 
+
+
+def pizza_ordering(database, item_count, order)
+	item_count += 1
+	order[item_count] = []
+sizes = database[:piz_sizes]
+crusts = database[:piz_crusts]
+sauces = database[:piz_sauces]
+meats = database[:piz_meat]
+veggies = database[:piz_veggies]
+special_tops = database[:piz_special_tops]
+piz_value = [sizes, crusts, sauces, meats, veggies, special_tops]
+value_name = ["Pizza Size", "Pizza Crust", "Pizza Sauce", "Pizza Meat", "Pizza Veggie", "Special Topping"]
+
+	for count in (0...piz_value.length)
+
+		system "clear"
+		system "cls"
+		selection = 0
+
+		puts "Select your pizza #{value_name[count]} from the following:"
+		puts ""
+		for x in (0...piz_value[count].length)
+
+			puts "#{x+1} - #{piz_value[count][x][0]}" 
+			#" $#{piz_value[count][1]}"
+		end
+
+			until selection.between?(1, piz_value[count].length)
+				puts "Enter the number that is next to your selection"
+				selection = gets.chomp
+				selection = selection.to_i 
+			end	
+
+			if piz_value[count] == sizes 
+				order[item_count].push(piz_value[count][selection - 1][0])
+				order[item_count].push(piz_value[count][selection - 1][1])
+			else
+				order[item_count].push(piz_value[count][selection - 1][0])
+			end
+	end
+
+
+	order[item_count].unshift('Pizza')
+	puts order 
+
+	return [order, item_count]
+	puts order 
+end
 
 
 order = Hash.new
@@ -121,6 +196,10 @@ while continue == "yes"
 	category = main()
 	if category == 5
 		order, item_count = dessert_order(order, database, item_count)
+	elsif category == 1
+		order, item_count = pizza_ordering(database, item_count, order)
+	elsif category == 3
+		order, item_count = sides_order(order, database, item_count)	
 	end
 	continue = add_item(order)
 end
@@ -142,84 +221,80 @@ puts order
 
 
 
-pizza_size = []
-crust_type = []
-meat_selection = []
-veggies_selection = []
-sauce_selection = []
-topping_selction = []
+# pizza_size = []
+# crust_type = []
+# meat_selection = []
+# veggies_selection = []
+# sauce_selection = []
+# topping_selction = []
 
 
+# 	size_sel = 0
+# 	until size.include? size_sel do 
+# 		puts "Pick the size for Pizza #{p} from the following sizes"
+# 		puts size
+# 		size_sel = gets.chomp
+# 		puts " "
+# 	end	
+# 		pizza_size.push (size_sel)
+# 		puts " "
 
+# #Select the crust for each pizza
+# 	crust_sel = 0
+# 	until crust.include? crust_sel do 
+# 		puts "Pick the crust for Pizza #{p} from the following types"
+# 		puts crust
+# 		crust_sel = gets.chomp
+# 		puts " "
+# 	end	
+# 		crust_type.push (crust_sel)
+# 		puts " "
 
-def pizza_ordering()
+# #Select the meat for each pizza
+# 	meats_sel = 0
+# 	until meats.include? meats_sel do 
+# 		puts "Pick the meat for Pizza #{p} from the following types"
+# 		puts meats
+# 		meats_sel = gets.chomp
+# 		puts " "
+# 	end	
+# 		meat_selection.push (meats_sel)
+# 		puts " "
 
-	size_sel = 0
-	until size.include? size_sel do 
-		puts "Pick the size for Pizza #{p} from the following sizes"
-		puts size
-		size_sel = gets.chomp
-		puts " "
-	end	
-		pizza_size.push (size_sel)
-		puts " "
+# #Select the veggies for each pizza
+# 	veggie_sel = 0
+# 	until veggies.include? veggie_sel do 
+# 		puts "Pick the veggie for Pizza #{p} from the following types"
+# 		puts veggies
+# 		veggie_sel = gets.chomp
+# 		puts " "
+# 	end	
+# 		veggies_selection.push (veggie_sel)
+# 		puts " "
 
-#Select the crust for each pizza
-	crust_sel = 0
-	until crust.include? crust_sel do 
-		puts "Pick the crust for Pizza #{p} from the following types"
-		puts crust
-		crust_sel = gets.chomp
-		puts " "
-	end	
-		crust_type.push (crust_sel)
-		puts " "
+# #Select the sauce for each pizza
+# 	sauce_sel = 0
+# 	until special_sauce.include? sauce_sel do 
+# 		puts "Pick the sauce for Pizza #{p} from the following types"
+# 		puts special_sauce
+# 		sauce_sel = gets.chomp
+# 		puts " "
+# 	end	
+# 		sauce_selection.push (sauce_sel)
+# 		puts " "
+# end
 
-#Select the meat for each pizza
-	meats_sel = 0
-	until meats.include? meats_sel do 
-		puts "Pick the meat for Pizza #{p} from the following types"
-		puts meats
-		meats_sel = gets.chomp
-		puts " "
-	end	
-		meat_selection.push (meats_sel)
-		puts " "
-
-#Select the veggies for each pizza
-	veggie_sel = 0
-	until veggies.include? veggie_sel do 
-		puts "Pick the veggie for Pizza #{p} from the following types"
-		puts veggies
-		veggie_sel = gets.chomp
-		puts " "
-	end	
-		veggies_selection.push (veggie_sel)
-		puts " "
-
-#Select the sauce for each pizza
-	sauce_sel = 0
-	until special_sauce.include? sauce_sel do 
-		puts "Pick the sauce for Pizza #{p} from the following types"
-		puts special_sauce
-		sauce_sel = gets.chomp
-		puts " "
-	end	
-		sauce_selection.push (sauce_sel)
-		puts " "
-end
-
-def show_pizzas(num_p, pizza_size, crust_type, meat_selection, veggies_selection, sauce_selection)
-	for c in 1..num_p
-		puts "Pizza #{c}" 
-		puts pizza_size[c-1]
-		puts crust_type[c-1]
-		puts meat_selection[c-1]
-		puts veggies_selection[c-1]
-		puts sauce_selection[c-1]
-		puts " "
-	end
-end
+# def show_pizzas(num_p, pizza_size, crust_type, meat_selection, veggies_selection, sauce_selection)
+# 	for c in 1..num_p
+# 		puts "Pizza #{c}" 
+# 		puts pizza_size[c-1]
+# 		puts crust_type[c-1]
+# 		puts meat_selection[c-1]
+# 		puts veggies_selection[c-1]
+# 		puts sauce_selection[c-1]
+# 		puts " "
+# 	end
+# end
 
 #show_pizzas(num_p, pizza_size, crust_type, meat_selection, veggies_selection, sauce_selection)
 
